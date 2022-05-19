22c9f6225bb32b124b27ebfc52418818838a9eeb
AnneAugust606 <82916149+gretchyn-hickman@users.noreply.github.com>
2022-05-19 15:10:34 -0600

    everything works

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import axios from 'axios';
 import "./App.css";
 import NasaPhoto from './NasaPhoto';
 
-const dummyData = {
-date: "2022-05-19",
-explanation: "Recorded on May 15/16 this sequence of exposures follows the Full Moon during a total lunar eclipse as it arcs above treetops in the clearing skies of central Florida. A frame taken every 5 minutes by a digital camera shows the progression of the eclipse over three hours. The bright lunar disk grows dark and red as it glides through planet Earth's shadow. In fact, counting the central frames in the sequence measures the roughly 90 minute duration of the total phase of this eclipse. Around 270 BC, the Greek astronomer Aristarchus also measured the duration of total lunar eclipses, but probably without the benefit of digital watches and cameras. Still, using geometry he devised a simple and impressively accurate way to calculate the Moon's distance in terms of the radius of planet Earth, from the eclipse duration.",
-hdurl: "https://apod.nasa.gov/apod/image/2205/TLE_2022-05-16-02-59-35s.jpg",
-title: "A Digital Lunar Eclipse",
-}
+// const dummyData = {
+// date: "2022-05-19",
+// explanation: "Recorded on May 15/16 this sequence of exposures follows the Full Moon during a total lunar eclipse as it arcs above treetops in the clearing skies of central Florida. A frame taken every 5 minutes by a digital camera shows the progression of the eclipse over three hours. The bright lunar disk grows dark and red as it glides through planet Earth's shadow. In fact, counting the central frames in the sequence measures the roughly 90 minute duration of the total phase of this eclipse. Around 270 BC, the Greek astronomer Aristarchus also measured the duration of total lunar eclipses, but probably without the benefit of digital watches and cameras. Still, using geometry he devised a simple and impressively accurate way to calculate the Moon's distance in terms of the radius of planet Earth, from the eclipse duration.",
+// hdurl: "https://apod.nasa.gov/apod/image/2205/TLE_2022-05-16-02-59-35s.jpg",
+// title: "A Digital Lunar Eclipse",
+// }
 
 function App() {
   const [data, setData] = useState();
