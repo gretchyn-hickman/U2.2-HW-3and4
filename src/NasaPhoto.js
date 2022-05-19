14f108986dd0f98132563566e2ee0afb79beee7f
AnneAugust606 <82916149+gretchyn-hickman@users.noreply.github.com>
2022-05-19 16:22:21 -0600

    Used Styled Components

--- src/NasaPhoto.js
+++ src/NasaPhoto.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import styled from "styled-components";
 
 
+const PhotoWrap = styled.div`
+  justify-content: center;
+`;
+
+const ParaDiv = styled.div`
+  justify-content: center;
+  background-color: lightblue;
+  border-radius: 3px;
+  border: 2px solid rgb(100, 100, 100);
+  width: 80%;
+  margin: 0 auto;
+  font-family: "Times New Roman", Times, serif;
+`;
 
 const NasaPhoto = (props => {
     return (
-        <div className="nasa-photo-wrapper">
-            <h3>{props.photo.title}</h3>
+        <PhotoWrap>
+            <h1>{props.photo.title}</h1>
             <p>{props.photo.date}</p>
             <img src={props.photo.hdurl}/>
+            <ParaDiv>
+            <h3>Summary:</h3>
             <p>{props.photo.explanation}</p>
-        </div>
+            </ParaDiv>
+        </PhotoWrap>
     )
 });
 
